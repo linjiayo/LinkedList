98dbd7a4e065abaa72983c3c351d230a040f09f6
Jiayong <jiagains@gmail.com>
2022-07-23 23:29:08 -0400

    add LinkedList implementation and test

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,152 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    static final class Node<T> {
+        T item;
+        Node<T> next;
+
+        public Node(T item) {
+            this.item = item;
+            this.next = null;
+        }
+        public Node() {
+        }
+    }
+
+    private Node<T> head;
+
+    public SinglyLinkedList() {
+        head = null;
+    }
+
+    public Node<T> getListHead() {
+        return head;
+    }
+
+    public void add(T item) {
+        Node<T> node = new Node(item);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tmp = head;
+        head = node;
+        head.next = tmp;
+    }
+
+    public void append(T item) {
+        Node<T> node = new Node<>(item);
+        this.append(node);
+    }
+
+    public void append(Node<T> node) {
+        if (head == null) {
+            head = node;
+            return;
+        }
+
+        if (head.next == null) {
+            head.next = node;
+            return;
+        }
+
+        // traverse linked list
+        Node<T> tail = head.next;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    public Node<T> get(int index) {
+        int count = 0;
+        if (head == null) {
+            return null;
+        }
+
+        Node<T> node = head;
+        while (node != null || count <= index) {
+            if (count == index) {
+                return node;
+            }
+            node = node.next;
+            count++;
+        }
+        return null;
+    }
+
+    public int size() {
+        int count = 0;
+        for (Node<T> node = head; node != null; node = node.next) {
+            count++;
+        }
+        return count;
+    }
+
+    public int find(T item) {
+        Node<T> node = head;
+        int index = 0;
+        while (node != null) {
+            if (node.item == item || node.item.equals(item)) {
+                return index;
+            }
+            index++;
+            node = node.next;
+        }
+        return -1;
+    }
+
+    public boolean contains(T item) {
+        return this.find(item) > -1;
+    }
+
+    public void remove(int index) {
+        Node<T> node = head;
+        Node<T> prev = null;
+        int count = 0;
+        if (index == 0) {
+            head = head.next;
+            return;
+        }
+        while (node != null && count < index) {
+            prev = node;
+            node = node.next;
+            count++;
+        }
+        prev.next = prev.next.next;
+    }
+
+    /** Deep copy of linked list **/
+    public SinglyLinkedList<T> copy() {
+        SinglyLinkedList<T> deepCopy = new SinglyLinkedList<>();
+
+        for (Node<T> node = head; node != null; node = node.next) {
+            deepCopy.append(new Node<>(node.item));
+        }
+        return deepCopy;
+    }
+
+    /** Merge sort **/
+    public void sort() {
+    }
+
+    public void reverse() {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        Node<T> headCopy = head;
+        Node<T> reverse = null;
+        while (headCopy != null) {
+            Node<T> second = headCopy.next;
+            headCopy.next = reverse;
+            reverse = headCopy;
+            headCopy = second;
+
+        }
+        head = reverse;
+    }
+
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,163 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    SinglyLinkedList<String> linkedList;
+    String[] str = new String[] { "1", "2", "3", "4"};
+
+    @BeforeEach
+    void testConstructor() {
+        linkedList = new SinglyLinkedList<>();
+    }
+    @Test
+    void add() {
+        String str1 = "test";
+        String expected = "test2";
+
+        linkedList.add(str1);
+        linkedList.add(expected);
+
+        String actual = linkedList.getListHead().item;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    void append() {
+        String str1  = "test";
+        String str2 = "test2";
+        String expected = "test3";
+
+        linkedList.append(str1);
+        linkedList.append(str2);
+        linkedList.append(expected);
+
+        String actual = linkedList.get(linkedList.size() -1).item;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSize() {
+        for (String s : str) {
+            linkedList.add(s);
+        }
+        Integer expected = 4;
+
+        Integer actual = linkedList.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFind() {
+        for (String s : str) {
+            linkedList.append(s);
+        }
+        Integer expectedPresent = 2;
+        Integer expectedNotPresent = -1;
+
+        Integer actualPresent = linkedList.find("3");
+        Integer actualNotPresent = linkedList.find("One");
+
+        Assert.assertEquals(expectedPresent, actualPresent);
+        Assert.assertEquals(expectedNotPresent, actualNotPresent);
+    }
+    @Test
+    public void testRemoveHead() {
+        for (String s : str) {
+            linkedList.append(s);
+        }
+
+        linkedList.remove(0);
+
+        Assertions.assertEquals(3, linkedList.size());
+    }
+
+    @Test
+    public void testRemoveMiddle() {
+        for (String s : str) {
+            linkedList.append(s);
+        }
+
+        linkedList.remove(1);
+        Integer expectedIndex = -1;
+
+        String actual = linkedList.get(1).item;
+        Integer actualIndex = linkedList.find("2");
+
+        Assert.assertEquals("3", actual);
+        Assert.assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void testRemoveLast() {
+        for (String s : str) {
+            linkedList.append(s);
+        }
+        linkedList.remove(3);
+
+        Integer expectedIndex = -1;
+
+        Integer actualIndex = linkedList.find("4");
+
+        Assert.assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void testContains() {
+        for (String s : str) {
+            linkedList.append(s);
+        }
+
+        Boolean expectedFound = true;
+        Boolean expectedNoItem = false;
+
+        Boolean actualFound = linkedList.contains("1");
+        Boolean actualNoItem = linkedList.contains("0");
+
+        Assert.assertEquals(expectedFound, actualFound);
+        Assert.assertEquals(expectedNoItem, actualNoItem);
+    }
+
+    @Test
+    public void testDeepCopy() {
+        for (String s : str) {
+            linkedList.append(s);
+        }
+
+        SinglyLinkedList<String> copy = linkedList.copy();
+        String expectedFirst = "1";
+        String expectedLast = "4";
+
+        String actualFirst = copy.get(0).item;
+        String actualLast = copy.get(copy.size() -1).item;
+
+
+        Assert.assertNotEquals(linkedList, copy);
+        Assert.assertEquals(expectedFirst, actualFirst);
+        Assert.assertEquals(expectedLast, actualLast);
+    }
+
+    @Test
+    public void testReverse() {
+        for (String s : str) {
+            linkedList.append(s);
+        }
+        SinglyLinkedList<String> rev = linkedList.copy();
+        rev.reverse();
+
+        for (int i = 0; i < linkedList.size(); i++) {
+            Assert.assertEquals(linkedList.get(i).item, rev.get(linkedList.size() - i - 1).item);
+        }
+    }
+
+
 }
